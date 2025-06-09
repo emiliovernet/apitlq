3456809059c1093dd9430a1eb798479f3fc3e5e3
Emilio Vernet <evernet@buenosaires.gob.ar>
2025-06-09 12:31:52 -0300

    Prueba jobs continuos #2

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,12 @@ Route::get('/dispatch-jobs', function () {
     }
     return "Dispatched 10 jobs!";
 });
+
+Route::get('/simulate-load', function () {
+    while (true) {
+        for ($i = 0; $i < 10; $i++) {
+            ProcessData::dispatch();
+        }
+        sleep(1); // 10 jobs por segundo
+    }
+});
